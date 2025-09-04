e59042bf07234a85fb0c96d471f5d650224f6220
skperry99 <sperry939@gmail.com>
2025-09-04 15:27:17 -0500

    Create testdb_user_management.sql

---
+++ sql/testdb_user_management.sql
@@ -0,0 +1,26 @@
+INSERT INTO users (username, firstName, lastName, age)
+VALUES
+ ('asmith', 'Alice', 'Smith', 20),
+ ('bdoe', 'Bob', 'Doe', 33),
+ ('cjohnson', 'Charlie', 'Johnson', 41),
+ ('dlee', 'David', 'Lee', 19),
+ ('ejones', 'Eve', 'Jones', 30),
+ ('fkim', 'Frank', 'Kim', 15),
+ ('gwhite', 'Grace', 'White', 22),
+ ('hwang', 'Henry', 'Wang', 19),
+ ('ijones', 'Ivy', 'Jones', 50),
+ ('jdoe', 'John', 'Doe', 65),
+ ('klee', 'Kevin', 'Lee', 62),
+ ('lchen', 'Linda', 'Chen', 24),
+ ('mlee', 'Michael', 'Lee', 29),
+ ('nkim', 'Nancy', 'Kim', 18),
+ ('ojohnson', 'Oscar', 'Johnson', 40),
+ ('pchen', 'Peter', 'Chen', 55),
+ ('qwang', 'Quincy', 'Wang', 28),
+ ('rjones', 'Rachel', 'Jones', 37),
+ ('slee', 'Sam', 'Lee', 21),
+ ('tkim', 'Tina', 'Kim', 26);
+ SELECT 
+    *
+FROM
+    users
